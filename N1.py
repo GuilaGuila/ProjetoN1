03cc429cecf8175d5cffac7141109accd12aa6be
Thiago Campos <thiagocampos2002@gmail.com>
2021-09-07 17:00:14 -0300

    Modificando as condicoes da função Path, adicionado a condicao do portal, e ajustes finais

--- N1.py
+++ N1.py
@@ -1,12 +1,16 @@
+import random
 
-
-def Path(dungeon , choice): 
-    if(choice == 1):
+def Path(dungeon, choice): 
+    if(choice == 1 and dungeon != 6):
         return dungeon + 1
-    elif(choice == 2):
+    elif(choice == 2 and dungeon != 8):
         return dungeon + 2
+    elif(choice == 2 and dungeon == 8):
+        print("Você entrou no portal e caiu em uma sala aleatória!")
+        return random.randrange(1, 6)
     else:
         print("Esta opção é inválida!")
+        return dungeon
 
 def Room(dungeon):
     if(dungeon == 6):
@@ -20,17 +24,26 @@ def Room(dungeon):
         print("[2] - Caminho Preto")
 
 dungeon = 1 
-
 i = 0
 
 while(dungeon != 9):
     Room(dungeon)
-    choice = int(input("Digite o numero do seu caminho "))
+    choice = int(input("Digite o numero do seu caminho: "))
+    print()
     dungeon = Path(dungeon, choice)
     i += 1
 
 if(dungeon == 9 and i < 7):
-    print("Parabéns você ganhou")
+    print("Parabéns você ganhou! Encontrou a sala 9 antes de esgotar os movimentos!")
+    print("  ___________")
+    print(" '._==_==_=_.'")
+    print(" .-\:      /-.")
+    print("| (|:.     |) |")
+    print(" '-|:.     |-'")
+    print("   \::.    /")
+    print("    '::. .'")
+    print("      ) (")
+    print("    _.' '._")
+    print("   '''''''''")
 else:
     print("Você perdeu! Chegou a sala 9, porém usou mais de 6 movimentos")
-    
