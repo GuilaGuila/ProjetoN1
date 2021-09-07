824a0ed485d5b4e345a0688e05416aec31b369d5
GuilaGuila <guilhermes.oliveira62@gmail.com>
2021-09-07 15:37:36 -0300

    Criando função Path para definir próxima dungeon

--- N1.py
+++ N1.py
@@ -1,52 +1,11 @@
-sala = 1 
+dungeon = 1 
 
-#teste
-
-print("Você está na sala 1!")
-print("Escolha seu caminho:")
-print("1 - Caminho vermelho")
-print("2 - Caminho preto")
-caminho = int(input("Digite o número que conrresponde ao caninho escolhido: "))
-
-if(caminho == 1):
-    sala = sala + 1
-else:
-    sala = sala + 2
-
-
-while(sala !=9 and sala !=6 and sala!=8):
-    print("Você está na sala {}!".format(sala))
-    print("Escolha seu caminho:")
-    print("1 - Caminho vermelho")
-    print("2 - Caminho preto")
-    caminho = int(input("Digite o número que conrresponde ao caninho escolhido: "))
-        
-    if(caminho == 1):
-        sala = sala + 1
+def Path(dungeon , choice): 
+    if(choice == 1):
+        return dungeon + 1
+    elif(choice == 2):
+        return dungeon + 2
     else:
-        sala = sala + 2
-    
-    
-if(sala == 8 and caminho == 2):
-    print("Ah não! Parece que você entrou em um portal desconhecido!")
-    print("O portal te levou para uma sala aleatória!")   
-else:
-    pass   
+        print("Esta opção é inválida!")
 
-if(sala == 6):
-    print("Você está na sala {}!".format(sala))
-    print("O caminho vermelho dessa sala está fehcado, prossiga pelo caminho preto!") 
-    print("Escolha seu caminho:")
-    print("1 - Caminho vermelho")
-    print("2 - Caminho preto")
-    caminho = int(input("Digite o número que conrresponde ao caninho escolhido: "))
-else:
-    pass
 
-if(sala == 9):
-    print("Você chegou na sala 9! Essa é a ultima sala!")
-    print("Parabéns você ganhou!")
-else:
-    pass
-
-#sla
